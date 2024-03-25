351f7d0633153debbc3d37479133e63ab7d30ad9
Deepak Mewada <mewadadeepak367@gmail.com>
2024-03-25 21:13:26 +0530

    add new arrow image in pubcpec.yaml file and made new file  adv_basic.dart

--- lib/quiz_app.dart
+++ lib/quiz_app.dart
@@ -1,19 +1,23 @@
-
 import 'package:flutter/material.dart';
-void moveNextpage(){
 
-}
+void moveNextpage() {}
 void main(List<String> args) {
-  runApp(
-    MaterialApp(
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+  @override
+  Widget build(context) {
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Container(
-            decoration:const BoxDecoration(
+            decoration: const BoxDecoration(
               gradient: LinearGradient(
-                colors:[
-                   Color.fromARGB(255, 69, 13, 191),
-                   Color.fromARGB(255, 70, 34, 126),
+                colors: [
+                  Color.fromARGB(255, 69, 13, 191),
+                  Color.fromARGB(255, 70, 34, 126),
                 ],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
@@ -23,7 +27,11 @@ void main(List<String> args) {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Image.asset('assets/images/quiz-logo.png',height: 250,width: 250,),
+                  Image.asset(
+                    'assets/images/quiz-logo.png',
+                    height: 250,
+                    width: 250,
+                  ),
                   const SizedBox(height: 40),
                   const Text(
                     'Learn Flutter the Fun Way!',
@@ -33,21 +41,37 @@ void main(List<String> args) {
                     ),
                   ),
                   const SizedBox(height: 40),
-                  const OutlinedButton(
+                  OutlinedButton(
                     onPressed: moveNextpage,
-                     child: Text(
-                        'Start quiz',
-                        style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                      ),
-                    ),
+                     child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+            
+                        Image.asset('assets/images/Untitled.png',width: 30,height: 20,),
+                        const SizedBox(width: 12),
+                        const Text(
+                          'Start Quiz',
+                          style: TextStyle(
+                            color: Colors.white,
+                            // fontSize: 
+                          ),
+                        )
+                        
+                      ],
+                     ),
+                    // Text(
+                    //   '-> Start quiz',
+                    //   style: TextStyle(
+                    //     color: Color.fromARGB(255, 255, 255, 255),
+                    //   ),
+                    // ),
+                  ),
                 ],
               ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
